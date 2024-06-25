10f0e463ea08e38d364b5e0d5a8ea3329fcf9a7f
Glebson Santos <glebsonsantos@outlook.pt>
2024-06-25 13:31:27 -0300

    Entidades

---
+++ src/DesafioCodigoCerto/Desafio.API/Models/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace Desafio.API.Models
+{
+    public class Employee : EntityBase
+    {
+        public string Name { get; set; }
+        public DateTime DataNasc { get; set; }
+        public string Cpf { get; set; }
+    }
+}
---
+++ src/DesafioCodigoCerto/Desafio.API/Models/EntityBase.cs
@@ -0,0 +1,14 @@
+﻿namespace Desafio.API.Models
+{
+    public class EntityBase
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public EntityBase() 
+        { 
+            Id = Guid.NewGuid();
+            CreatedAt = DateTime.UtcNow;
+        }
+    }
+}
