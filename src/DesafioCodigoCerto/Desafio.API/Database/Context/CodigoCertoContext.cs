8023253e20a91f0faa3bca071e2c0a73c7e34fc1
Glebson Santos <glebsonsantos@outlook.pt>
2024-06-25 14:12:46 -0300

    Criado Context, Migration e Bd

---
+++ src/DesafioCodigoCerto/Desafio.API/Database/Context/CodigoCertoContext.cs
@@ -0,0 +1,12 @@
+﻿using Desafio.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Desafio.API.Database.Context
+{
+    public class CodigoCertoContext : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+
+        public CodigoCertoContext(DbContextOptions<CodigoCertoContext> opt) : base(opt) { }
+    }
+}
---
+++ src/DesafioCodigoCerto/Desafio.API/Database/Migrations/20240625170326_CreateDatabase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Desafio.API.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    DataNasc = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Cpf = table.Column<string>(type: "TEXT", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+        }
+    }
+}
